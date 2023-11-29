2dea38c1389d8e3294de4efb7d9618309a731c55
Ved Kothavade <ved@kothavade.com>
2023-11-29 04:28:36 -0500

    remove hybrid astro mode

--- astro.config.ts
+++ astro.config.ts
@@ -1,24 +1,23 @@
-import { defineConfig } from "astro/config";
-import vue from "@astrojs/vue";
-import mdx from "@astrojs/mdx";
-import solidJs from "@astrojs/solid-js";
-import sitemap from "@astrojs/sitemap";
-import unocss from "unocss/astro";
-import vesper from "./src/lib/vesper.json";
+import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
+import mdx from '@astrojs/mdx'
+import solidJs from '@astrojs/solid-js'
+import sitemap from '@astrojs/sitemap'
+import unocss from 'unocss/astro'
+import vesper from './src/lib/vesper.json'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.kothavade.com",
-  output: "hybrid",
+  site: 'https://www.kothavade.com',
   integrations: [mdx(), sitemap(), solidJs(), unocss({
-    injectReset: true
+    injectReset: true,
   }), vue()],
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   markdown: {
     shikiConfig: {
     // @ts-expect-error
       theme: vesper,
-      wrap: true
-    }
+      wrap: true,
+    },
   },
-});
+})
