cfc5411abf21485591782dd64a50146083d5f7b8
Ved Kothavade <ved@kothavade.com>
2023-11-30 23:06:42 -0500

    Update Link component to hide underline

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
-const props = defineProps<{ href: string }>()
+const props = defineProps<{ href: string; hideUnderline?: boolean }>()
 
 const isExternal = computed(() => props.href.startsWith('http') || props.href.includes('kothavade.com/gh'))
+
+const linkClasses = computed(() => {
+  const classes = ['underline', 'decoration-neutral-600', 'underline-offset-4', 'transition-colors', 'focus:(decoration-neutral-500 outline-offset-6)', 'hover:decoration-neutral-500']
+  if (props.hideUnderline)
+    classes.splice(classes.indexOf('underline'), 1)
+
+  return classes
+})
 </script>
 
 <template>
   <a
     :href="props.href" :target="isExternal ? '_blank' : '_self'"
-    class="underline decoration-neutral-600 underline-offset-4 transition-colors focus:(decoration-neutral-500 outline-offset-6) hover:decoration-neutral-500"
+    :class="linkClasses"
   >
     <slot />
   </a>
