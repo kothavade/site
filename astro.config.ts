ff3e9ce7d91c82818de42e1fa2bc3c71d00a51ae
Ved Kothavade <ved@kothavade.com>
2023-12-14 17:27:50 -0500

    vercel take 2

--- astro.config.ts
+++ astro.config.ts
@@ -1,22 +1,33 @@
-import { defineConfig } from 'astro/config'
-import vue from '@astrojs/vue'
-import mdx from '@astrojs/mdx'
-import solidJs from '@astrojs/solid-js'
-import sitemap from '@astrojs/sitemap'
-import unocss from 'unocss/astro'
-import vesper from './src/lib/vesper.json'
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import mdx from '@astrojs/mdx';
+import solidJs from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
+import unocss from 'unocss/astro';
+import vesper from './src/lib/vesper.json';
+
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.kothavade.com',
   integrations: [mdx(), sitemap(), solidJs(), unocss({
-    injectReset: true,
+    injectReset: true
   }), vue()],
   markdown: {
     shikiConfig: {
       // @ts-expect-error theme is weird
       theme: vesper,
-      wrap: true,
-    },
+      wrap: true
+    }
   },
-})
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+  })
+});
