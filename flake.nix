dd2b01b1c7639e7d207501497a8ee872a14a7fe5
Ved Kothavade <ved@kothavade.com>
2023-12-14 19:06:28 -0500

    switch from bun to pnpm

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.systems.url = "github:nix-systems/default";
+
+  outputs = { self, nixpkgs, flake-utils, systems, }:
+    flake-utils.lib.eachSystem (import systems) (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default =
+          # Need nodejs until https://github.com/oven-sh/bun/issues/6537 is in a release
+          pkgs.mkShell {
+            buildInputs = with pkgs; [ nodejs_18 nodePackages.pnpm ];
+          };
+      });
+}
